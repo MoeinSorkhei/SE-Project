3b468a5948452a395e7b0aa41ab2dabcac02904d
MoeinSorkhei <m.moein.sorkhei@gmail.com>
2016-06-20 14:37:52 +0430

    going for test

---
+++ Law.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SAD.Business_Layer;
+using System.Windows.Forms;
+
+namespace SAD
+{
+    public class Law
+    {
+        private DateTime startDate;
+        private DateTime finishDate;
+        private double minUnitPrice;
+        private double maxUnitPrice;
+        private int minCount;
+        //private int maxCount;
+        private string country;
+        private string kindOfImport;
+        private double minTotalPrice;
+        private List<string> merchandiseList;
+        private List<string> companies;
+        private List<string> licensesNeeded;
+
+        public Law(DateTime startDate, DateTime finishDate, double minUnitPrice, 
+                   double maxUnitPrice, int minCount/*, int maxCount*/, string country, string kindOfImport
+                   , double minTotalPrice, List<string> merchandiseList, List<string> companies, List<string> licensesNeeded)
+        {
+            this.startDate = startDate;
+            this.finishDate = finishDate;
+            this.minUnitPrice = minUnitPrice;
+            this.maxUnitPrice = maxUnitPrice;
+            this.minCount = minCount;
+            //this.maxCount = maxCount;
+            this.country = country;
+            this.kindOfImport = kindOfImport;
+            this.merchandiseList = merchandiseList;
+            this.companies = companies;
+            this.licensesNeeded = licensesNeeded;
+            this.minTotalPrice = minTotalPrice;
+        }
+
+        public bool ConditionsMatched(Statement statement)
+        {
+            //licensesNeeded.Clear(); // This List Sould Not Be HERE AT ALLLLLLLL!!!!
+            //List<string> licenses = new List<string>();
+            //List<string> merchandises = new List<string>();
+            //List<string> companies = new List<string>();
+
+            //merchandises.Add("گازوییل");
+            //merchandises.Add("بنزین");
+            //merchandises.Add("قیر");
+            //licenses.Add("مجوز واردات فرآورده های نفتی");
+            //CreateNewLaw(DateTime.MinValue, DateTime.MaxValue, double.MinValue, double.MaxValue, int.MinValue,
+            //             String.Empty, double.MinValue, String.Empty, merchandises, companies, licenses);
+            //licenses = new List<string>();
+            //merchandises = new List<string>();
+            //companies = new List<string>();
+
+            //merchandises.Add("بشکه");
+            //merchandises.Add("ورق آهن");
+            //merchandises.Add("میلگرد");
+            //merchandises.Add("تیرآهن");
+            //licenses.Add("مجوز واردات کالای خارجی تولید داخل");
+            //licenses.Add("مجوز واردات کالاهای فلزی");
+            //CreateNewLaw(DateTime.MinValue, DateTime.MaxValue, 100, 10000, 100,
+            //             String.Empty, double.MinValue, String.Empty, merchandises, companies, licenses);
+            //licenses = new List<string>();
+            //merchandises = new List<string>();
+            //companies = new List<string>();
+
+
+            //merchandises.Add("شامپو");
+            //merchandises.Add("کرم");
+            //licenses.Add("مجوز سلامت بهداشتی کالا های متفرقه");
+            //CreateNewLaw(new DateTime(2010, 1, 1), DateTime.MaxValue, double.MinValue, double.MaxValue, int.MinValue,
+            //             "چین", double.MinValue, String.Empty, merchandises, companies, licenses);
+            //licenses = new List<string>();
+            //merchandises = new List<string>();
+            //companies = new List<string>();
+
+
+            //companies.Add("هیوندای");
+            //companies.Add("کیا");
+            //licenses.Add("مجوز  واردات خودرو های خارجی");
+            //CreateNewLaw(DateTime.MinValue, DateTime.MaxValue, double.MinValue, double.MaxValue, int.MinValue,
+            //            String.Empty, 1000000, String.Empty, merchandises, companies, licenses);
+            //licenses = new List<string>();
+            //merchandises = new List<string>();
+            //companies = new List<string>();
+
+
+            //licenses.Add("مجوز واردات کالاهای لوکس");
+            //licenses.Add("مجوز سلامت فیزیکی کالاها");
+            //licenses.Add("عدم مغایرت با اهداف اقتصاد مقاومتی");
+            //CreateNewLaw(new DateTime(2014, 1, 1), new DateTime(2017, 1, 1), 100000, 1000000, int.MinValue,
+            //             "سوئیس", double.MinValue, "هوایی", merchandises, companies, licenses);
+            //licenses = new List<string>();
+            //merchandises = new List<string>();
+            //companies = new List<string>();
+
+            ////MessageBox.Show("law size: " + laws.Count);
+            if (statement.GetIssuanceDate() > startDate && statement.GetIssuanceDate() < finishDate
+                && (String.IsNullOrEmpty(country) ? true : (statement.GetCountry() == country))
+                && (String.IsNullOrEmpty(kindOfImport) ? true : statement.GetKindOfImport() == kindOfImport)
+                && (this.minTotalPrice <= statement.GetWholeValue()))
+            {
+                bool minCountIsLimited;
+                bool minUnitPriceIsLimted;
+                bool companyIsLimited;
+
+                if (this.merchandiseList.Count == 0) // no limitation for name of goods
+                {
+                    foreach (MerchandiseInStatement statementMerchandise in statement.GetMerchandiseList())
+                    {
+                        minCountIsLimited = false/*true*/;
+                        minUnitPriceIsLimted = false /*true*/;
+                        companyIsLimited = false /*true*/;
+
+                        if (statementMerchandise.GetUnitPrice() > minUnitPrice
+                            && statementMerchandise.GetUnitPrice() < maxUnitPrice)
+                            minUnitPriceIsLimted = true /*false*/;
+                        //MessageBox.Show("false because unit price of: " + statementMerchandise.GetName());
+                        //statementMerchandise.GetUnitPrice().ToString() + " " + law.GetMinUnitPrice().ToString());
+
+                        if (statementMerchandise.GetCount() > minCount
+                           /*&& statementMerchandise.GetCount() <= law.GetMaxCount()*/)
+                            minCountIsLimited = true;
+
+                        if (companies.Count == 0) // law states no company
+                            companyIsLimited = true;
+                        else if (companies.Contains(statementMerchandise.GetCompany())) // law states companies
+                            companyIsLimited = true;
+
+                        MessageBox.Show("flags: " + minUnitPriceIsLimted.ToString() + " " + minCountIsLimited.ToString() + " " + companyIsLimited.ToString());
+                        if (minUnitPriceIsLimted && minCountIsLimited && companyIsLimited)
+                            return true;
+                    }
+                }
+                else
+                {
+                    foreach (string lawMerchandiseName in merchandiseList)
+                    {
+                        if (statement.hasMerchandiseWithName(lawMerchandiseName))
+                        {
+                            minCountIsLimited = false/*true*/;
+                            minUnitPriceIsLimted = false /*true*/;
+                            companyIsLimited = false /*true*/;
+
+                            MerchandiseInStatement currentmerchandise = statement.getMerchandiseWithName(lawMerchandiseName);
+                            MessageBox.Show("Got one equal name");
+                            if (currentmerchandise.GetUnitPrice() > minUnitPrice
+                                && currentmerchandise.GetUnitPrice() < maxUnitPrice)
+                                minUnitPriceIsLimted = true;
+                            if (currentmerchandise.GetCount() > minCount
+                                /*&& statementMerchandise.GetCount() <= law.GetMaxCount()*/)
+                                minCountIsLimited = true;
+
+                            //MessageBox.Show(companies.Count.ToString());
+                            //MessageBox.Show(companies[0].ToString());
+                            //MessageBox.Show("join: " + String.IsNullOrEmpty(String.Join("", companies)).ToString());
+                            //MessageBox.Show(companies.ToString());
+                            //if (companies.Count == 0)
+                            if (String.IsNullOrEmpty(String.Join("", companies)))
+                                companyIsLimited = true;
+                            else if (companies.Contains(currentmerchandise.GetCompany()))
+                                companyIsLimited = true;
+                            MessageBox.Show("flags: " + minUnitPriceIsLimted.ToString() + " " + minCountIsLimited.ToString() + " " + companyIsLimited.ToString());
+                            if (minUnitPriceIsLimted && minCountIsLimited && companyIsLimited)
+                                return true;
+                        }
+                    }
+                }
+                //MessageBox.Show("condition checking: priceLimited: " + minUnitPriceIsLimted.ToString() + "      minCountLimited: " + minCountIsLimited.ToString() + "       companyLimited: " + companyIsLimited.ToString());
+                //if (minUnitPriceIsLimted && minCountIsLimited && companyIsLimited)
+                    //return true;
+            }
+            //MessageBox.Show("not matched at first");
+            return false;
+        }
+        public bool HasEnyMerchandiseWithNames(List<MerchandiseInStatement> merchandiseItems)
+        {
+            foreach(MerchandiseInStatement item in merchandiseItems)
+            {
+                if (merchandiseList.Contains(item.GetName()))
+                    return true;
+            }
+            return false;
+        }
+        public DateTime GetStartDate()
+        {
+            return this.startDate;
+        }
+
+        public DateTime GetFinishDate()
+        {
+            return this.finishDate;
+        }
+
+        public double GetMinUnitPrice()
+        {
+            return this.minUnitPrice;
+        }
+
+        public double GetMaxUnitPrice()
+        {
+            return this.maxUnitPrice;
+        }
+
+        public int GetMinCount()
+        {
+            return this.minCount;
+        }
+
+        //public int GetMaxCount()
+        //{
+        //    return this.maxCount;
+        //}
+
+        public string GetCountry()
+        {
+            return this.country;
+        }
+
+        public string GetImportType()
+        {
+            return this.kindOfImport;
+        }
+
+        public double GetMinTotalPrice()
+        {
+            return this.minTotalPrice;
+        }
+
+        public List<string> GetLicenses()
+        {
+            return this.licensesNeeded;
+        }
+
+        public List<string> GetCompanies()
+        {
+            return this.companies;
+        }
+
+        public List<string> GetMerchandiseList()
+        {
+            return this.merchandiseList;
+        }
+    }
+}
---
+++ LawDAO.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SAD.DAO_Layer
+{
+    public class LawDAO
+    {
+        public List<Law> GetLaws()
+        {
+            List<Law> laws = new List<Law>();
+            string query = "select * from dbo.Law";
+            DBConnection dbConnection = new DBConnection();
+            List<List<Object>> queryResults = new List<List<Object>>();
+            queryResults = dbConnection.RunSelectQuery(query);
+
+            for (int i = 0; i < queryResults.Count; i++)
+            {
+                //MessageBox.Show(Convert.ToString(queryResults[i][1]));
+                //MessageBox.Show(String.IsNullOrEmpty(Convert.ToString(queryResults[i][1])).ToString());
+                //MessageBox.Show(DateTime.MaxValue.ToString());
+                DateTime startDate = String.IsNullOrEmpty(queryResults[i][1].ToString()) ? DateTime.MinValue 
+                                    : Convert.ToDateTime(queryResults[i][1]);
+                DateTime finishDate = String.IsNullOrEmpty(queryResults[i][2].ToString()) ? DateTime.MaxValue
+                                    : Convert.ToDateTime(queryResults[i][2]);
+                double minUnitPrice = String.IsNullOrEmpty(queryResults[i][3].ToString()) ? double.MinValue
+                                    : Convert.ToDouble(queryResults[i][3]);
+                double maxUnitPrice = String.IsNullOrEmpty(queryResults[i][4].ToString()) ? double.MaxValue
+                                    : Convert.ToDouble(queryResults[i][4]);
+                int minCount = String.IsNullOrEmpty(queryResults[i][5].ToString()) ? int.MinValue
+                                    : Convert.ToInt32(queryResults[i][5]);
+                string country = Convert.ToString(queryResults[i][6]);
+                string kindOfImport = Convert.ToString(queryResults[i][7]);
+                double minTotalPrice = String.IsNullOrEmpty(queryResults[i][8].ToString()) ? double.MinValue
+                                    : Convert.ToDouble(queryResults[i][8]);
+                string merchandises = String.IsNullOrEmpty(queryResults[i][9].ToString()) ? ""
+                                    : Convert.ToString(queryResults[i][9]);
+                //MessageBox.Show(String.IsNullOrEmpty(queryResults[i][10].ToString()).ToString());
+                string companies = String.IsNullOrEmpty(queryResults[i][10].ToString()) ? "" :
+                    Convert.ToString(queryResults[i][10]);
+                string licenses = String.IsNullOrEmpty(queryResults[i][11].ToString()) ? "" 
+                    : Convert.ToString(queryResults[i][11]);
+
+                string[] merchandiseList = merchandises.Split('#');
+                //MessageBox.Show("String to be splitted: " + companies.Length);
+                string[] companiesList = companies.Split('#');
+                MessageBox.Show("in dao: company array count: " + companiesList.Length);
+                string[] licensesNeeded = licenses.Split('#');
+                laws.Add(new Law(startDate, finishDate, minCount, maxUnitPrice, minCount, country, kindOfImport,
+                    minCount, new List<string>(merchandiseList), new List<string>(companiesList), new List<string>(licensesNeeded)));
+            }
+            //MessageBox.Show("laws count in DB: " + laws.Count.ToString());
+            //foreach (Law l in laws)
+                //MessageBox.Show("in DAO--- Laws: " + l.GetStartDate().ToString() + "\n" + l.GetFinishDate().ToString() + "\n" + l.GetMinCount().ToString() + "\n" + l.GetMerchandiseList().ToString() + " \n" + l.GetLicenses().ToString());
+            //MessageBox.Show("in LawDAO: all Laws:" + String.Join("\n", laws));
+            return laws;
+        }
+        public void AddNewLaw(Law law)
+        {
+            string merchadises = String.Join("#", law.GetMerchandiseList().ToArray());
+            string companies = String.Join("#", law.GetCompanies().ToArray());
+            string licenses = String.Join("#", law.GetLicenses().ToArray());
+
+            //for (int i = 0; i < law.GetMerchandiseList().Count; i++)
+            //{
+            //    merchadises += law.GetMerchandiseList()[i];
+            //    if (i != law.GetMerchandiseList().Count - 1)
+            //        merchadises += '#';
+            //}
+
+            //for (int i = 0; i < law.GetCompanies().Count; i++)
+            //{
+            //    companies += law.GetCompanies()[i];
+            //    if (i != law.GetCompanies().Count - 1)
+            //        companies += '#';
+            //}
+
+            //for (int i = 0; i < law.GetLicenses().Count; i++)
+            //{
+            //    licenses += law.GetLicenses()[i];
+            //    if (i != law.GetLicenses().Count - 1)
+            //        licenses += '#';
+            //}
+
+            string query = "insert into dbo.Law values" + "('" + Convert.ToString(law.GetStartDate()) + "', '"
+                           + Convert.ToString(law.GetFinishDate()) + "', '" + Convert.ToString(law.GetMinUnitPrice()) + "', '"
+                           + Convert.ToString(law.GetMaxUnitPrice()) + "', '" + Convert.ToString(law.GetMinCount()) + "', N'"
+                           + Convert.ToString(law.GetCountry()) + "', N'" + Convert.ToString(law.GetImportType()) + "', '"
+                           + Convert.ToString(law.GetMinTotalPrice()) + "', N'" + merchadises + "', N'" + companies + "', N'" + licenses + "')";
+            MessageBox.Show("in in LawDAO: new law added successfully");
+        }
+    }
+}
